f545bb0d5a43f6a6b9f81d62b381e5b9e2b7c606
I-linmeng-I <1364876516@qq.com>
2024-03-27 00:08:18 +0800

    重新划分区域

--- common.h
+++ common.h
@@ -55,10 +55,10 @@ struct card_sort {
 #define LOCATION_GRAVE		0x10		//
 #define LOCATION_REMOVED	0x20		//
 #define LOCATION_EXTRA		0x40		//
-#define LOCATION_EXILE      0x50       //除外区
-#define LOCATION_DAMAGE     0x65       //伤害区
-#define LOCATION_ORDER      0x70       //指令区
-#define LOCATION_SPARE      0x90       //备用格子（用于怪物箱等
+#define LOCATION_EXILE      0x60       //除外区
+#define LOCATION_DAMAGE     0x80       // 伤害区
+#define LOCATION_ORDER      0x90       // 指令区
+#define LOCATION_SPARE      0xA0       // 备用格子
 #define LOCATION_OVERLAY	0x80		//
 #define LOCATION_ONFIELD	0x0c		//
 #define LOCATION_FZONE		0x100		//
